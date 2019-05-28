6f75f5146ee0b02d7a45a88c15477b8d697319a2
Leonid Medovyy <leonid@logik.works>
2019-05-28 15:14:34 +0530

    setup pug and less

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+const erb =  require('./loaders/erb')
+const coffee = require('./loaders/coffee')
+const pug =  require('./loaders/pug')
+const less =  require('./loaders/less')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('erb', erb)
+environment.loaders.append('coffee', coffee)
+environment.loaders.append('less', less)
+environment.loaders.append('pug', pug)
 
 module.exports = environment
